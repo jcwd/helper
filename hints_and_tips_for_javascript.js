a237344768d9ccb25cd70d8f95afc919a35233f2
jcwd <jon.collins71@gmail.com>
2016-06-27 11:37:48 +0200

    adds work of june 27th

--- hints_and_tips_for_javascript.js
+++ hints_and_tips_for_javascript.js
@@ -16,3 +16,32 @@ consol.log("Hello world!");
 $("#header").append()
 // we tell the system that we are selecting header via the $("#header")
 // we tell it we want to add to the html by using the action .append()
+
+// slicing a string
+var str1 = 'The morning is upon us.';
+var str2 = str1.slice(4, -2);
+
+console.log(str2); // OUTPUT: morning is upon u
+// the above will remove 4 characters from the left and 2 from the right of the string. thus "The " and "s.
+
+// to manipulate a string here is an example
+var s = "audacity";
+
+var udacityizer = function(s) {
+// first we can slice out the beginning 91st character)
+s = s.slice(1);
+// then we can replace characters within the string to model it to our needs
+s = s.replace(/u/g, "U");
+// this can also be done with the code below by selecting and transforming the index in a string
+s = s[0].toUpperCase()
+
+// arrays
+var skills.length = ["awesomeness", "programming","teaching","JS"]
+console.log(skills)
+
+
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
