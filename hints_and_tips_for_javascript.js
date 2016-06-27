b74ac8f3be0541b2a38efd2e207fa5ec7c3f7561
jcwd <jon.collins71@gmail.com>
2016-06-27 07:57:45 +0200

    initial commit of helper files

---
+++ hints_and_tips_for_javascript.js
@@ -0,0 +1,18 @@
+// <!-- javascript -->
+// <!-- in order to link to the javascript we need to address it in the header of the HTML -->
+<head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="js/jQuery.js"></script>
+</head>
+
+// <!-- printing to Javascript (usefull for debugging) -->
+consol.log("Hello world!");
+// consol.log prints to the consol
+// always use Semi Colons when ending statements in JavaScript
+// enclosing something in () tellsJavascrip that you do not wish a return value
+// enclosin in "" tells JavaScript that you have a string
+
+// appending info to an html element
+$("#header").append()
+// we tell the system that we are selecting header via the $("#header")
+// we tell it we want to add to the html by using the action .append()
